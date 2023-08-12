e7a4fc78a9a86f50b0f2e46509d06880e9814a59
giordano.cassini <giordano.cassini@gmail.com>
2023-08-12 17:43:03 -0300

    feat: 2 testes completados compra "com carrinho vazio" e compra "com quantidade zero"

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,21 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+
+        if(itens.length === 0) return 'Não há itens no carrinho de compra!';
+
+        const chaves = [];
+        const valores = [];
+        itens.forEach(element => {
+            const [chave, valorString] = element.split(',');
+            const valor = Number(valorString);
+            chaves.push(chave);
+            valores.push(valor);
+        });
+
+        if (valores.includes(0)) return "Quantidade inválida!";
+
+
         return "";
     }
 
